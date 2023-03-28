982fd25eaa44563ed09b898ffc7c6af3368fe02b
VoDuyKhang-1911062456 <voduykhang312001@gmail.com>
2023-03-28 16:35:45 +0700

    add form and export to excel

--- FirebaseESP32/src/main/java/com/mycompany/firebaseesp32/frmServer.java
+++ FirebaseESP32/src/main/java/com/mycompany/firebaseesp32/frmServer.java
@@ -29,11 +29,21 @@ import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import java.awt.Desktop;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.concurrent.ExecutionException;
+import javax.swing.JFileChooser;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 /**
  *
@@ -42,7 +52,7 @@ import java.util.concurrent.ExecutionException;
 public class frmServer extends javax.swing.JFrame {
 
     RealtimeFirebase fbs = null;
-    
+
     DatabaseReference ref = null;
 
     private static JSONObject jObject = null;
@@ -56,6 +66,10 @@ public class frmServer extends javax.swing.JFrame {
      */
     public frmServer() throws IOException {
         initComponents();
+        fillDataJTable("1911062456", "Vo Duy Khang", "March 22, 2023 at 12:45:09PM UTC+7", (byte) 1);
+        fillDataJTable("1911062456", "Vo Duy Khang", "March 22, 2023 at 12:45:09PM UTC+7", (byte) 1);
+        fillDataJTable("1911062456", "Vo Duy Khang", "March 22, 2023 at 12:45:09PM UTC+7", (byte) 1);
+        fillDataJTable("1911062456", "Vo Duy Minh", "March 22, 2023 at 12:45:09PM UTC+7", (byte) 1);
 
     }
 
@@ -84,11 +98,9 @@ public class frmServer extends javax.swing.JFrame {
         }
 
     }
-    
-    
-    
+
     public void insertDataFireStore() throws FileNotFoundException, IOException, InterruptedException, ExecutionException {
-       
+
         this.fbs = new RealtimeFirebase();
 
         DocumentReference docRef = fbs.getData().collection("users").document("alovelace1");
@@ -148,6 +160,11 @@ public class frmServer extends javax.swing.JFrame {
         listData = new javax.swing.JTextArea();
         jButton2 = new javax.swing.JButton();
         cbbTime = new javax.swing.JComboBox<>();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -176,35 +193,75 @@ public class frmServer extends javax.swing.JFrame {
             }
         });
 
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "MSSV", "Name", "Time", "Status"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable1);
+
+        jButton3.setText("Save");
+
+        jButton4.setText("Export to Excel File");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        jLabel1.setText("ĐIỂM DANH SINH VIÊN");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(23, 23, 23)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGap(18, 18, 18)
                         .addComponent(cbbTime, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(67, 67, 67)
-                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 259, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(21, Short.MAX_VALUE))
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 357, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(22, 22, 22)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jButton3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton4))
+                .addContainerGap(14, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(359, 359, 359))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(15, 15, 15)
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(34, 34, 34)
+                .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 223, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(cbbTime, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(cbbTime))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2, javax.swing.GroupLayout.DEFAULT_SIZE, 41, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 459, Short.MAX_VALUE)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jButton3)
+                        .addGap(13, 13, 13)
+                        .addComponent(jButton4)
+                        .addGap(18, 18, 18)
+                        .addComponent(jButton2))
+                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
@@ -244,14 +301,14 @@ public class frmServer extends javax.swing.JFrame {
             Date date = new Date(epoch * 1000);
             SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd-MM-yyyy - HH:mm:SS");
             String dates = DATE_FORMAT.format(date);
-            
+
             // Filter the time after the Firebase is initialized
             Calendar initCal = Calendar.getInstance();
             initCal.setTime(this.firebaseLastConnectedTime);
-            
+
             Calendar scanCal = Calendar.getInstance();
             scanCal.setTime(date);
-            
+
             if (initCal.compareTo(scanCal) >= 0) {
                 continue;
             }
@@ -289,7 +346,7 @@ public class frmServer extends javax.swing.JFrame {
         if (this.isConnected) {
             this.ref.onDisconnect();
             this.fbs.db.goOffline();
-            
+
             this.fbs = null;
             this.jButton1.setText("Connect");
             this.isConnected = false;
@@ -315,6 +372,47 @@ public class frmServer extends javax.swing.JFrame {
     private void cbbTimeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbbTimeActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_cbbTimeActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        try {
+            JFileChooser jFileChooser = new JFileChooser();
+            jFileChooser.showSaveDialog(this);
+            File saveFile = jFileChooser.getSelectedFile();
+
+            if (saveFile != null) {
+                saveFile = new File(saveFile.toString() + ".xlsx");
+                Workbook wb = new XSSFWorkbook();
+                Sheet sheet = wb.createSheet("diemdanh");
+
+                Row rowCol = sheet.createRow(0);
+                for (int i = 0; i < jTable1.getColumnCount(); i++) {
+                    Cell cell = rowCol.createCell(i);
+                    cell.setCellValue(jTable1.getColumnName(i));
+                }
+
+                for (int j = 0; j < jTable1.getRowCount(); j++) {
+                    Row row = sheet.createRow(j + 1);
+                    for (int k = 0; k < jTable1.getColumnCount(); k++) {
+                        Cell cell = row.createCell(k);
+                        if (jTable1.getValueAt(j, k) != null) {
+                            cell.setCellValue(jTable1.getValueAt(j, k).toString());
+                        }
+                    }
+                }
+                FileOutputStream out = new FileOutputStream(new File(saveFile.toString()));
+                wb.write(out);
+                wb.close();
+                out.close();
+                openFile(saveFile.toString());
+            } else {
+                JOptionPane.showMessageDialog(null, "Error al generar archivo");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException io) {
+            System.out.println(io);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
 
     public void updateTextArea(String data) {
         listData.setText(data);
@@ -360,12 +458,53 @@ public class frmServer extends javax.swing.JFrame {
         });
     }
 
+//    Jtable to excel
+    public void fillDataJTable(String mssv, String name, String time, byte status) {
+        String[] columns = new String[]{
+            "Mssv", "Name", "Time", "Status"
+        };
+        String sts = "";
+        if (status != 1) {
+            sts = "Tre";
+        } else {
+            sts = "Dung Gio";
+        }
+//        Object[][] data = new Object[][]{
+//            {mssv, name, time, sts}, //            {"1911060595","Pham Trung Tin","March 22, 2023 at 12:45:09PM UTC+7","1"},
+//        //            {"1911060565","Le Van Thang","March 22, 2023 at 12:47:09PM UTC+7","1"},
+//        //            {"1911062443","Le Van Huy","March 22, 2023 at 12:50:09PM UTC+7","1"},
+//        //            
+//        };
+        Object data[] = new Object[] {mssv, name, time, sts};
+        String data2[] = {mssv, name, time, sts};
+        
+//        DefaultTableModel model = (new DefaultTableModel(data, columns))jTable1.getModel();
+//        jt.setModel(model);
+//        JTable jt = new JTable(model);
+//        model.addRow(data);
+        
+    }
+
+    public void openFile(String file) {
+        try {
+            File path = new File(file);
+            Desktop.getDesktop().open(path);
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JComboBox<String> cbbTime;
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JProgressBar jProgressBar1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTable jTable1;
     private javax.swing.JTextArea listData;
     // End of variables declaration//GEN-END:variables
 }
